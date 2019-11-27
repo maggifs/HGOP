312e49c12d940b9c0af75c865b37b61a9c4c3b09
brutzki <emilnewel@gmail.com>
2019-11-27 14:33:46 +0000

    deploy script rdy

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change directory to 1 higher
+cd ..
+
+# Destroy any existing Terraform instance
+terraform destroy -auto-approve
+
+# Create a new one
+terraform apply -auto-approve
+
+# Run the initialization script on the new instance
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
+
