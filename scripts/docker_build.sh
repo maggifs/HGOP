fc6dcee03bd12be5dc20a856d171b92c2c127b65
brutzki <emilnewel@gmail.com>
2019-12-02 14:55:20 +0000

    Added error check for docker_build script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,6 +2,8 @@
 
 GIT_COMMIT=$1
 
-docker build -t emiln17/hgop:$GIT_COMMIT itemrepository/
-
-# TODO exit on error if any command fails
+if docker build -t emiln17/hgop:$GIT_COMMIT itemrepository/; then
+    echo "Docker build succeeded"
+else
+    echo "Docker build failed"
+fi
