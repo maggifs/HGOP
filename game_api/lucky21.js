2407a364dc514d754744d9abafbedb2700a6595a
magnuss17 <magnuss17@ru.is>
2019-12-04 13:24:29 +0000

    eslint fix lucky21.js

--- game_api/lucky21.js
+++ game_api/lucky21.js
@@ -1,37 +1,37 @@
 module.exports = (deck, dealer) => {
   dealer.shuffle(deck);
-  let card0 = dealer.draw(deck);
-  let card1 = dealer.draw(deck);
-  let state = {
+  const card0 = dealer.draw(deck);
+  const card1 = dealer.draw(deck);
+  const state = {
     deck: deck,
     dealer: dealer,
     cards: [card0, card1],
     // The card that the player thinks will exceed 21.
-    card: undefined
+    card: undefined,
   };
   return {
     state: state,
     // Is the game over (true or false).
     // Is the game finished.
-    isGameOver: game => {
-      //TESTED
-      if (game.playerWon(game) || game.getTotal(game) >= 21 || game.state.card)
+    isGameOver: (game) => {
+      if (game.playerWon(game) || game.getTotal(game) >= 21 || game.state.card) {
         return true;
+      }
       return false;
     },
     // Has the player won (true or false).
-    playerWon: game => {
+    playerWon: (game) => {
       if (game.getTotal(game) > 21 && game.state.card) return true;
-      if (game.getTotal(game) == 21 && game.state.card == undefined)
+      if (game.getTotal(game) == 21 && game.state.card == undefined) {
         return true;
+      }
       return false;
     },
     // The highest score the cards can yield without going over 21 (integer).
-    getCardsValue: game => {
-      //TESTED
+    getCardsValue: (game) => {
       let counter = 0;
       let total = 0;
-      let cards = game.state.cards;
+      const cards = game.state.cards;
       for (i = 0; i < cards.length; i++) {
         let card = parseInt(cards[i].substr(0, 2));
         if (card > 10) {
@@ -53,8 +53,8 @@ module.exports = (deck, dealer) => {
       return total;
     },
     // The value of the card that should exceed 21 if it exists (integer or undefined).
-    getCardValue: game => {
-      let card = game.state.card;
+    getCardValue: (game) => {
+      const card = game.state.card;
       if (card) {
         cardVal = parseInt(card.substr(0, 2));
         if (cardVal > 10) {
@@ -65,9 +65,9 @@ module.exports = (deck, dealer) => {
       return card;
     },
     // The cards value + the card value if it exits (integer).
-    getTotal: game => {
-      let cardsValue = game.getCardsValue(game);
-      let cardValue = game.getCardValue(game);
+    getTotal: (game) => {
+      const cardsValue = game.getCardsValue(game);
+      const cardValue = game.getCardValue(game);
 
       if (cardValue) {
         return cardsValue + cardValue;
@@ -75,22 +75,21 @@ module.exports = (deck, dealer) => {
       return cardsValue;
     },
     // The player's cards (array of strings).
-    getCards: game => {
+    getCards: (game) => {
       return game.state.cards;
     },
     // The player's card (string or undefined).
-    getCard: game => {
+    getCard: (game) => {
       return game.state.card;
     },
     // Player action (void).
-    guess21OrUnder: game => {
-      // TODO
+    guess21OrUnder: (game) => {
       game.state.cards.push(deck.pop());
     },
     // Player action (void).
-    guessOver21: game => {
-      let card = game.state.deck.pop();
+    guessOver21: (game) => {
+      const card = game.state.deck.pop();
       game.state.card = card;
-    }
+    },
   };
 };
