c2a8782895e5c32e7ca28cb89cadb45f75705132
brutzki <emilnewel@gmail.com>
2019-12-05 16:48:01 +0000

    sync

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-JENKINS_URL="http://54.242.184.100/"
+# Our Jenkins instance url
+JENKINS_URL=54.242.184.100
 
+# Copy credentials to Jenkins machine and move it to the correct path.
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
