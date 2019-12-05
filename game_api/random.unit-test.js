71dcdb8f3491ae08bad8a7f3d55eeb3b5f5c1935
brutzki <emilnewel@gmail.com>
2019-12-05 15:15:05 +0000

    many changes

--- game_api/random.unit-test.js
+++ game_api/random.unit-test.js
@@ -1,3 +1,14 @@
+function newRandom(randomReturnValues) {
+  let i = 0;
+  return {
+    randomInt: (min, max) => {
+      return randomReturnValues[i++];
+    }
+  };
+}
+
 describe('Random', () => {
-    
-})
+  test('', () => {});
+  test('', () => {});
+  test('', () => {});
+});
