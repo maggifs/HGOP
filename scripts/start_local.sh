ceeb4a4c1901b544cc6eb201001042dcdcdef048
magnuss17 <magnuss17@ru.is>
2019-12-10 17:53:58 +0000

    insertDate added to migration

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-docker build ../game_api/ -t emiln17/hgop:62586c7b3517d2ec10743d95d6fb967586c98b97
-GIT_COMMIT=62586c7b3517d2ec10743d95d6fb967586c98b97 docker-compose up
+docker build ../game_api/ -t emiln17/hgop:e42478556062d5ce204858d8427da1e7d4d7499e
+GIT_COMMIT=e42478556062d5ce204858d8427da1e7d4d7499e docker-compose up
