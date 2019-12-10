6e5d4e594a0e3eeaf5b6625597c0ac41d6ec90a1
brutzki <emilnewel@gmail.com>
2019-12-10 15:18:10 +0000

    added http:// to url

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,4 +8,4 @@ ENV=$2
 ./scripts/jenkins_deploy.sh $GIT_COMMIT $ENV
 TEST_URL=$(cd /var/lib/jenkins/terraform/hgop/$ENV && terraform output public_ip)
 cd game_api
-API_URL=$TEST_URL:3000 npm run test:$ENV
+API_URL=http://$TEST_URL:3000 npm run test:$ENV
