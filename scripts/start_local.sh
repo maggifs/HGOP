ba8c7204e6b7ed3c09b9ba1140385a3b642575d6
brutzki <emilnewel@gmail.com>
2019-12-10 22:34:37 +0000

    start scriptu update

--- scripts/start_local.sh
+++ scripts/start_local.sh
@@ -2,5 +2,8 @@
 
 set -euxo pipefail
 
-docker build ../game_api/ -t emiln17/hgop:c20f70725626f5e660967090fcfd20401c5a5937
-GIT_COMMIT=c20f70725626f5e660967090fcfd20401c5a5937 docker-compose up
+docker build -t emiln17/hgop:dev game_api/  
+(cd /game_client && npm build)
+docker build -t emiln17/game_client:dev game_client/
+
+API_URL=localhost GIT_COMMIT=dev docker-compose up
