65547904346740716f2799ebe67667c3509cc79b
brutzki <emilnewel@gmail.com>
2019-12-11 13:29:41 +0000

    fixed build script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 GIT_COMMIT=$1
 
 docker build -t emiln17/hgop:$GIT_COMMIT game_api/;
-docker build -t emiln17/hgop:$GIT_COMMIT game_client/;
+docker build -t emiln17/game_client:$GIT_COMMIT game_client/;
